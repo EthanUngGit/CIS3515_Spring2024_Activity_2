21e5737e4c1ddcc07e8f5cd0d1311c991c3ec043
EthanUngGit <ethan.ung@temple.edu>
2024-02-06 12:13:09 -0500

    Change TextView's text size to the number selected in the Spinner

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -16,14 +16,34 @@ class MainActivity : AppCompatActivity() {
 
         /* TODO Step 1: Populate this array */
         //val numberArray = Array Of Numbers
-        val numberArray = 1..100
+        val numberArray = (2..100 step 2).toList().toTypedArray()
 
         /* TODO Step 2: Create adapter to display items from array in Spinner */
         //spinner.adapter = ArrayAdapter...
-        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, numberArray)
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_dropdown_item_1line,
+            numberArray
+        )
         spinner.adapter = adapter
 
         // TODO Step 3: Change TextView's text size to the number selected in the Spinner */
         //spinner.onItemSelectedListener = object: ...
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem = numberArray[position]
+                displayTextView.textSize = selectedItem.toFloat()
+                TODO("Not yet implemented")
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
     }
 }
